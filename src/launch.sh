b3dcb11637f1ffd41e3c6e6ae60ff864ae014021
Ankush Rana <ankush.rana@bsc.es>
2024-06-19 13:11:23 +0200

    WIP

--- src/launch.sh
+++ src/launch.sh
@@ -8,6 +8,8 @@
 #SBATCH --cpus-per-task=20
 #SBATCH --qos=acc_bscls
 #SBATCH --partition=acc
+# --- IMPORTANT: DO NOT DELETE OR MODIFY THE FOLLOWING LINE ---
+# --- IMPORTANT: DO NOT DELETE OR MODIFY THE ABOVE LINE ---
 
 # --- IMPORTANT: DO NOT DELETE OR MODIFY THE FOLLOWING LINE ---
 module load singularity
@@ -19,3 +21,8 @@ JOB_LOGS_PATH="$JOB_PATH/logs/"
 # Your existing code here
 echo "Launch python script"
 singularity exec --nv --no-home --pwd /src --bind $JOB_PATH:/src --bind /gpfs:/gpfs $GPFS_SINGULARITY_IMAGE_REGISTRY_PATH/python-jupyter.sif python code/script.py > $JOB_LOGS_PATH/script.log 2>&1
+
+
+# --- IMPORTANT: DO NOT DELETE OR MODIFY THE FOLLOWING LINE ---
+sleep 30d
+# --- IMPORTANT: DO NOT DELETE OR MODIFY THE ABOVE LINE ---
