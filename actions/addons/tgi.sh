c59410261ea90c96a1a969fe221675e5b827ff4f
Ankush Rana <ankush.rana@bsc.es>
2024-06-26 12:51:26 +0200

    WIP

--- actions/addons/tgi.sh
+++ actions/addons/tgi.sh
@@ -4,4 +4,4 @@ nohup singularity run --nv \
     $GPFS_TGI_SINGULARITY  \
     --model-id /data/$GPFS_TGI_MODEL \
     --hostname 0.0.0.0 \
-    --port 8080 --max-input-length 8000 --max-batch-prefill-tokens 8000 --max-total-tokens 12000 > $JOB_PATH/logs/tgi.log 2>&1 &
+    --port 8080 --max-input-length 8000 --max-batch-prefill-tokens 8000 --max-total-tokens 12000 > $JOB_LOGS_PATH/tgi.log 2>&1 &
