b6d345544f4aaddba613b074bc4875fd11e1cf37
Ankush Rana <ankush.rana@bsc.es>
2024-06-26 12:38:16 +0200

    WIP

--- actions/addons/tgi.sh
+++ actions/addons/tgi.sh
@@ -2,9 +2,6 @@ echo "Launch tgi endpoint"
 nohup singularity run --nv \
     --bind $GPFS_MODELS_REGISTRY_PATH:/data  \
     $GPFS_TGI_SINGULARITY  \
-    --model-id /data/Mixtral-8x7B-Instruct-v0.1 \
+    --model-id /data/$GPFS_TGI_MODEL \
     --hostname 0.0.0.0 \
-    --port 8080 \
-    --max-input-length 8000 \
-    --max-batch-prefill-tokens 8000 \
-    --max-total-tokens 12000 > $JOB_PATH/logs/tgi.log 2>&1 &
+    --port 8080 --max-input-length 8000 --max-batch-prefill-tokens 8000 --max-total-tokens 12000 > $JOB_PATH/logs/tgi.log 2>&1 &
